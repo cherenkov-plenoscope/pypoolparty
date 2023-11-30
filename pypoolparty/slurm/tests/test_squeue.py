e8fe28acd64fdd570b66917cfa5048fee20e5c89
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-11-30 15:19:54 +0100

    adopt slurm components

---
+++ pypoolparty/slurm/tests/test_squeue.py
@@ -0,0 +1,23 @@
+import pypoolparty
+import importlib
+import os
+
+
+def test_parse_squeue():
+    pypoolparty_dir = pypoolparty.utils.resources_path()
+    stdout_path = os.path.join(
+        pypoolparty_dir,
+        "slurm",
+        "tests",
+        "resources",
+        "squeue_format_all.stdout",
+    )
+
+    with open(stdout_path) as f:
+        o = f.read()
+
+    d = pypoolparty.slurm.call._parse_stdout_format_all(o)
+
+    print(d[18])
+
+    assert len(d) == 231
