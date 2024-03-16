254ff42c7d63899f6da57a1664270b158716a3a9
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-03-16 13:02:26 +0100

    more force when calling stupid slurm

--- pypoolparty/slurm/call.py
+++ pypoolparty/slurm/call.py
@@ -1,6 +1,7 @@
 import json_line_logger
 import subprocess
 import tempfile
+import random
 import os
 import time
 from .. import utils
@@ -16,6 +17,8 @@ def sbatch(
     clusters=None,
     sbatch_path="sbatch",
     timeout=None,
+    timecooldown=1.0,
+    max_num_retry=25,
 ):
     if logger is None:
         logger = json_line_logger.LoggerStdout()
@@ -31,11 +34,22 @@ def sbatch(
     for argument in script_arguments:
         cmd += [argument]
 
-    try:
-        subprocess.check_output(cmd, stderr=subprocess.STDOUT, timeout=timeout)
-    except subprocess.CalledProcessError as e:
-        logger.critical(str(e))
-        raise
+    numtry = 0
+    while True:
+        raise_if_too_often(
+            numtry=numtry, max_num_retry=max_num_retry, logger=logger
+        )
+        try:
+            numtry += 1
+            logger.debug("calling sbatch, num. tries = {:d}".format(numtry))
+            subprocess.check_output(
+                cmd, stderr=subprocess.STDOUT, timeout=timeout
+            )
+            break
+        except Exception as bad:
+            logger.warning("Problem in sbatch()")
+            logger.warning(str(bad))
+            sleep(timecooldown=timecooldown, logger=logger)
 
 
 def scancel(
@@ -43,45 +57,37 @@ def scancel(
     scancel_path="scancel",
     timeout=None,
     timecooldown=1.0,
+    max_num_retry=25,
     logger=None,
 ):
     if logger is None:
         logger = json_line_logger.LoggerStdout()
 
-    t = 0
+    cmd = [scancel_path, "--name", str(jobname)]
+
+    numtry = 0
     while True:
+        raise_if_too_often(
+            numtry=numtry, max_num_retry=max_num_retry, logger=logger
+        )
         try:
-            _scancel(
-                scancel_path=scancel_path,
-                jobname=jobname,
-                timeout=timeout,
-                logger=logger,
+            numtry += 1
+            logger.debug("calling scancel, num. tries = {:d}".format(numtry))
+            subprocess.check_output(
+                cmd, stderr=subprocess.STDOUT, timeout=timeout
             )
             break
-        except KeyboardInterrupt:
-            raise
         except Exception as bad:
             logger.warning("Problem in scancel()")
             logger.warning(str(bad))
-            time.sleep(timecooldown)
-
-
-def _scancel(scancel_path, jobname, timeout, logger=None):
-    if logger is None:
-        logger = json_line_logger.LoggerStdout()
-
-    try:
-        cmd = [scancel_path, "--name", str(jobname)]
-        subprocess.check_output(cmd, stderr=subprocess.STDOUT, timeout=timeout)
-    except subprocess.CalledProcessError as e:
-        logger.critical(str(e))
-        raise
+            sleep(timecooldown=timecooldown, logger=logger)
 
 
 def squeue(
     squeue_path="squeue",
     timeout=None,
     timecooldown=1.0,
+    max_num_retry=25,
     logger=None,
     debug_dump_path=None,
 ):
@@ -107,6 +113,9 @@ def squeue(
 
     numtry = 0
     while True:
+        raise_if_too_often(
+            numtry=numtry, max_num_retry=max_num_retry, logger=logger
+        )
         try:
             numtry += 1
             logger.debug("calling squeue, num. tries = {:d}".format(numtry))
@@ -116,14 +125,10 @@ def squeue(
                 logger=logger,
             )
             break
-        except KeyboardInterrupt:
-            logger.warning("KeyboardInterrupt")
-            raise
         except Exception as bad:
             logger.warning("problem in _squeue_format_all_stdout()")
             logger.warning(str(bad))
-            logger.warning("waiting for {:f}s".format(float(timecooldown)))
-            time.sleep(timecooldown)
+            sleep(timecooldown=timecooldown, logger=logger)
 
     logger.debug("parsing stdout into list of dicts")
 
@@ -196,3 +201,16 @@ def _squeue_format_all_stdout(squeue_path="squeue", timeout=None, logger=None):
 
     logger.debug("len(stdout) = {:d}".format(len(stdout)))
     return stdout
+
+
+def sleep(timecooldown, logger):
+    delta_time = timecooldown * random.uniform(1 / 2, 3 / 2)
+    logger.warning("waiting for {:f}s".format(float(delta_time)))
+    time.sleep(delta_time)
+
+
+def raise_if_too_often(numtry, max_num_retry, logger):
+    if numtry >= max_num_retry:
+        msg = "Aborting. Too many retries."
+        logger.critical(msg)
+        raise RuntimeError(msg)
