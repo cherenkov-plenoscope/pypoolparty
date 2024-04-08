f4c2467c2c6d141c70ea48ffe17be2f57727bc21
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-04-08 19:23:31 +0200

    more logger, calling of sbatch in own module

--- pypoolparty/slurm/array/__init__.py
+++ pypoolparty/slurm/array/__init__.py
@@ -1,6 +1,7 @@
 from . import making_script
 from . import mapping
 from . import reducing
+from . import calling
 from ... import utils
 
 import subprocess
@@ -11,15 +12,45 @@ import shutil
 
 
 class Pool:
+    """
+    A pool of compute resources on a distributed compute cluster
+    using slurm's sbatch --array.
+    """
+
     def __init__(
         self,
-        processes=None,
+        num_simultaneously_running_tasks=None,
         python_path=None,
         polling_interval=5.0,
         work_dir=None,
         keep_work_dir=False,
         verbose=False,
     ):
+        """
+        Parameters
+        ----------
+        num_simultaneously_running_tasks : int or None
+            Up to this many tasks will run in parallel.
+        python_path : str or None
+            The python path to be used on the computing-cluster's worker-nodes
+            to execute the worker-node's python-script.
+        polling_interval : float or None
+            The time in seconds to wait before polling squeue again while
+            waiting for the jobs to finish.
+        work_dir : str
+            The directory path where the tasks, the results and the
+            worker-node-script is stored.
+        keep_work_dir : bool
+            When True, the working directory will not be removed.
+        verbose : bool
+            If true, the pool will print the state of its jobs to stdout.
+
+        Returns
+        -------
+        pool : pypoolparty.slurm.array.Pool
+            A pool instance with a map() function.
+        """
+
         if python_path is None:
             self.python_path = utils.default_python_path()
         else:
@@ -29,7 +60,9 @@ class Pool:
         self.polling_interval = float(polling_interval)
         assert self.polling_interval > 0.0
         self.keep_work_dir = bool(keep_work_dir)
-        self.processes = processes
+        self.num_simultaneously_running_tasks = (
+            num_simultaneously_running_tasks
+        )
         self.verbose = bool(verbose)
 
     def __repr__(self):
@@ -39,7 +72,32 @@ class Pool:
         print("[pypoolparty]", utils.time_now_iso8601(), msg)
 
     def map(self, func, iterable):
-        tasks = iterable
+        """
+        Apply `func` to each element in `iterable`, collecting the results
+        in a list that is returned.
+
+        Parameters
+        ----------
+        func : function-pointer
+            Pointer to a function in a python-module. It must have both:
+            func.__module__
+            func.__name__
+        iterable : list
+            List of tasks. Each task must be a valid input to 'func'.
+
+        Returns
+        -------
+        results : list
+            Results. One result for each task.
+
+        Example
+        -------
+        results = pool.map(sum, [[1, 2], [2, 3], [4, 5], ])
+            [3, 5, 9]
+        """
+
+        tasks = iterable  # to be consistent with multiprocessing's pool.map.
+
         session_id = utils.session_id_from_time_now()
         if self.work_dir is None:
             work_dir = os.path.abspath(
@@ -49,18 +107,16 @@ class Pool:
             work_dir = os.path.abspath(self.work_dir)
 
         os.makedirs(work_dir)
-        if self.verbose:
-            self.print("start: {:s}".format(work_dir))
-
         logger = json_line_logger.LoggerFile(
             path=os.path.join(work_dir, "log.jsonl")
         )
 
-        logger.debug("Starting map()")
-        logger.debug("python path: {:s}".format(self.python_path))
-        logger.debug("polling-interval: {:f}s".format(self.polling_interval))
+        _start_msg = "start: {:s}".format(work_dir)
+        logger.debug(_start_msg)
+        if self.verbose:
+            self.print(_start_msg)
 
-        logger.debug("Making script.")
+        logger.debug("Making script...")
         script_path = os.path.join(work_dir, "script.py")
         script_content = making_script.make(
             func_module=func.__module__,
@@ -70,29 +126,33 @@ class Pool:
         )
         utils.write_text(path=script_path, content=script_content)
         utils.make_path_executable(path=script_path)
+        logger.debug("Making script: done.")
 
+        logger.debug("Mapping {:d} tasks...".format(len(tasks)))
         mapping.write_tasks_to_work_dir(work_dir=work_dir, tasks=tasks)
-        logger.debug("Wrote {:d} tasks into work_dir.".format(len(tasks)))
+        logger.debug("Mapping {:d} tasks: done.".format(len(tasks)))
 
-        logger.debug("Calling sbatch --array.")
-        call_sbatch_array(
+        logger.debug("Calling sbatch --array...")
+        calling.sbatch_array(
             work_dir=work_dir,
             jobname=session_id,
             start_task_id=0,
             stop_task_id=len(iterable) - 1,
-            num_simultaneously_running_tasks=self.processes,
+            num_simultaneously_running_tasks=self.num_simultaneously_running_tasks,
             logger=logger,
         )
+        logger.debug("Calling sbatch --array: done.")
 
-        logger.debug("Prepare reducing of results.")
+        logger.debug("Preparing reduction of results...")
         reducer = reducing.Reducer(work_dir=work_dir)
         num_tasks_returned_last_poll = -1
+        logger.debug("Preparing reduction of results: done.")
 
-        logger.debug("Wait for tasks to finish.")
+        logger.debug("Waiting for tasks to return...")
         while True:
             reducer.reduce()
 
-            poll_msg = "returned: {: 6d} / {:d}".format(
+            poll_msg = "tasks: {: 6d} / {:d}".format(
                 len(reducer.tasks_returned), len(tasks)
             )
             if len(reducer.tasks_exceptions) or len(reducer.tasks_with_stderr):
@@ -107,13 +167,17 @@ class Pool:
                     self.print(poll_msg)
 
             if len(reducer.tasks_returned) == len(tasks):
-                logger.debug("complete")
+                logger.debug("All tasks returned.")
                 break
 
             num_tasks_returned_last_poll = int(len(reducer.tasks_returned))
             time.sleep(self.polling_interval)
 
+        logger.debug("Waiting for tasks to return: done.")
+
+        logger.debug("Closing files to reduce results...")
         reducer.close()
+        logger.debug("Closing files to reduce results: done.")
 
         remove_this_work_dir = True
 
@@ -121,15 +185,22 @@ class Pool:
             remove_this_work_dir = False
 
         if len(reducer.tasks_exceptions) > 0:
+            logger.warning("Found exceptions, will keep work_dir.")
             remove_this_work_dir = False
 
         if len(reducer.tasks_with_stderr) > 0:
+            logger.warning("Found non empty stderr, will keep work_dir.")
             remove_this_work_dir = False
 
+        logger.debug("Reading results to return them...")
         out = reducing.read_task_results(
-            work_dir=work_dir, len_tasks=len(tasks), logger=logger,
+            work_dir=work_dir,
+            len_tasks=len(tasks),
+            logger=logger,
         )
+        logger.debug("Reading results to return them: done.")
 
+        logger.debug("Shuting down logger.")
         utils.shutdown_logger(logger=logger)
         del logger
 
@@ -137,60 +208,3 @@ class Pool:
             shutil.rmtree(work_dir)
 
         return out
-
-
-def call_sbatch_array(
-    work_dir,
-    jobname,
-    start_task_id,
-    stop_task_id,
-    num_simultaneously_running_tasks=None,
-    sbatch_path="sbatch",
-    timeout=None,
-    timecooldown=1.0,
-    max_num_retry=25,
-    logger=None,
-):
-    logger = utils.make_logger_to_stdout_if_none(logger)
-    assert start_task_id >= 0
-    assert stop_task_id >= stop_task_id
-
-    cmd = [sbatch_path]
-    if num_simultaneously_running_tasks is None:
-        cmd += ["--array", "{:d}-{:d}".format(start_task_id, stop_task_id)]
-    else:
-        num_simultaneously_running_tasks = int(
-            num_simultaneously_running_tasks
-        )
-        assert num_simultaneously_running_tasks > 0
-        cmd += [
-            "--array",
-            "{:d}-{:d}%{:d}".format(
-                start_task_id, stop_task_id, num_simultaneously_running_tasks
-            ),
-        ]
-
-    logger.debug("Call: " + str.join(" ", cmd))
-
-    cmd += ["--output", os.path.join(work_dir, "%a.stdout")]
-    cmd += ["--error", os.path.join(work_dir, "%a.stderr")]
-    cmd += ["--job-name", jobname]
-    cmd += [os.path.join(work_dir, "script.py")]
-
-    numtry = 0
-    while True:
-        utils.raise_if_too_often(
-            numtry=numtry, max_num_retry=max_num_retry, logger=logger
-        )
-        try:
-            numtry += 1
-            subprocess.check_output(
-                cmd, stderr=subprocess.STDOUT, timeout=timeout
-            )
-            break
-        except Exception as bad:
-            logger.warning(
-                "Problem calling sbatch, num. tries = {:d}".format(numtry)
-            )
-            logger.warning(str(bad))
-            utils.random_sleep(timecooldown=timecooldown, logger=logger)
